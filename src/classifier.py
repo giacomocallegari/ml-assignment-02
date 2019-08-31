323fa53e966965fc826216f3e92da0433dacdb30
giacomocallegari <giacomo.callegari@studenti.unitn.it>
2019-08-31 08:26:47 +0200

    Perform cross validation

--- src/classifier.py
+++ src/classifier.py
@@ -1,21 +1,28 @@
 import numpy as np
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.svm import SVC
 
 
 def load_data():
     """Loads the input data and the output targets from the specified path."""
 
-    # Path of the data.
+    print("")
+    print("*** DATA LOADING ***")
+
+    # Path of the CSV files.
     path = "..\\data\\ocr\\"
 
+    # Number of considered examples, for debug reasons. Use "None" to take the whole dataset.
+    train_size = 2000
+    test_size = 500
+
     # Load the input data.
-    print("Loading the input data...")
-    X_train = np.genfromtxt(path + "train-data.csv", delimiter=",")
-    X_test = np.genfromtxt(path + "test-data.csv", delimiter=",")
+    X_train = np.genfromtxt(path + "train-data.csv", delimiter=",")[:train_size]
+    X_test = np.genfromtxt(path + "test-data.csv", delimiter=",")[:test_size]
 
     # Load the output targets.
-    print("Loading the output targets...")
-    y_train = np.genfromtxt(path + "train-targets.csv", dtype="str")
-    y_test = np.genfromtxt(path + "test-targets.csv", dtype="str")
+    y_train = np.genfromtxt(path + "train-targets.csv", dtype="str")[:train_size]
+    y_test = np.genfromtxt(path + "test-targets.csv", dtype="str")[:test_size]
 
     print("Number of training examples: ", len(X_train))
     print("Number of test examples: ", len(X_test))
@@ -23,20 +30,57 @@ def load_data():
     return X_train, X_test, y_train, y_test
 
 
+def cross_validation(X_train, y_train):
+    """Performs k-fold cross validation to tune the model's parameters."""
+
+    print("")
+    print("*** CROSS VALIDATION ***")
+
+    # Define the cross validation process.
+    kf = KFold(n_splits=3, shuffle=True, random_state=42)
+
+    # Specify the candidate values.
+    gamma_values = [0.1, 0.05, 0.02, 0.01]
+    accuracy_scores = []
+
+    # Perform cross validation for each candidate value.
+    for gamma in gamma_values:
+
+        # Train the classifier.
+        clf = SVC(C=10, kernel='rbf', gamma=gamma)
+
+        # Compute the cross validation scores.
+        scores = cross_val_score(clf, X_train, y_train, cv=kf.split(X_train), scoring='accuracy')
+
+        # Compute the mean accuracy and save it.
+        accuracy_score = scores.mean()
+        accuracy_scores.append(accuracy_score)
+
+        print("gamma = ", gamma, " \tscore = ", accuracy_score)
+
+    # Get the gamma with highest mean accuracy.
+    best_index = np.array(accuracy_scores).argmax()
+    best_gamma = gamma_values[best_index]
+    print("The best gamma is ", best_gamma)
+
+    return best_gamma
+
+
 def main():
     """Main function."""
 
-    # Load the data
+    # Load the data.
     X_train, X_test, y_train, y_test = load_data()
 
-    # Perform cross validation
+    # Perform cross validation to obtain the optimal parameters.
+    gamma = cross_validation(X_train, y_train)
 
-    # Train the optimal classifier
+    # Train the optimal classifier.
 
-    # Test the classifier
+    # Test the classifier.
 
-    # Draw the learning curve
+    # Draw the learning curve.
 
 
-# Start the program
+# Start the program.
 main()
